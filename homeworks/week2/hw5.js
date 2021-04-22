83681bce336e7158628c12ffe850d1a56d59d5e3
Liu <gentle9278@gmail.com>
2021-04-23 00:37:28 +0800

    week2

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,33 @@
 function join(arr, concatStr) {
-  
+	
+	let result= ''
+	if(concatStr===''){
+		for(let i=0;i<arr.length;i++){
+			result+=(arr[i]+concatStr)
+				}	return result
+					  }	
+	else{
+		for(let i=0;i<arr.length;i++){
+			result+=(arr[i]+concatStr)
+				}	return result.slice(0,result.length-1)
+		}
+	
+	
 }
 
+
+
 function repeat(str, times) {
+    let result=''
+	for(let i=1;i<=times;i++){
+		result+=str
+	}return result
   
 }
+console.log(join([1, 2, 3], ''))
+console.log(join(["a", "b", "c"], "!"))
+console.log(join(["a", 1, "b", 2, "c", 3], ','))
 
 console.log(join(['a'], '!'));
 console.log(repeat('a', 5));
+console.log(repeat('yoyo', 2));
