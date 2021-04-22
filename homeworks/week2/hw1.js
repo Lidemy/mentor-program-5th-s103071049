8ec279e36393d4190704875dbaeead6a63a1f437
Liu <gentle9278@gmail.com>
2021-04-23 01:47:35 +0800

    week2

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,14 +1,10 @@
 function printStars(n) {
-	if(n>30||n<=0){
-		console.log("n的範圍超過設定值")
-		//給定 n（1<=n<=30）
-	}
-	else {
-		for(let i=1;i<=n;i++){
+	
+	for(let i=1;i<=n;i++){
 			console.log("*")
 
 		}
-	}
+	
 }
 
 
