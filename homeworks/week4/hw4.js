a423495dfbb9c19de289e2c681ec6e5394890ac4
Liu <gentle9278@gmail.com>
2021-05-05 13:35:34 +0800

    w

---
+++ homeworks/week4/hw4.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line
+const request = require('request')
+
+request(
+  // eslint-disable-next-line
+  options = {
+    url: 'https://api.twitch.tv/kraken/games/top',
+    headers: {
+      'Client-ID': 'ka1mgyrfu09rtyw1znuocyf6x0xciq',
+      // eslint-disable-next-line
+      'Accept': 'application/vnd.twitchtv.v5+json'
+    }
+  },
+
+  (error, response, body) => {
+    try {
+      const json = JSON.parse(body)
+      // eslint-disable-next-line
+      const length = json.top.length
+      for (let i = 0; i < length; i++) {
+        console.log(`${json.top[i].game.name} ${json.top[i].viewers}`)
+      }
+    } catch (error) {
+      console.log('error')
+    }
+  }
+)
