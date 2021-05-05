7aff91d8ad8fab236e4de40f1823df121a25e99f
Liu <gentle9278@gmail.com>
2021-05-05 10:23:11 +0800

    hw3

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -12,13 +12,16 @@ request(
     const json = JSON.parse(body)
     // eslint-disable-next-line
     const length = Object.keys(json).length
-
-    for (let i = 0; i < length; i++) {
-      console.log('============')
-      console.log(`國家 : ${json[i].name}`)
-      console.log(`首都 : ${json[i].capital}`)
-      console.log(`貨幣 : ${json[i].currencies[0].code}`)
-      console.log(`國碼 : ${json[i].callingCodes}`)
+    try {
+      for (let i = 0; i < length; i++) {
+        console.log('============')
+        console.log(`國家 : ${json[i].name}`)
+        console.log(`首都 : ${json[i].capital}`)
+        console.log(`貨幣 : ${json[i].currencies[0].code}`)
+        console.log(`國碼 : ${json[i].callingCodes}`)
+      }
+    } catch (error) {
+      console.log('找不到國家資訊')
     }
   }
 
