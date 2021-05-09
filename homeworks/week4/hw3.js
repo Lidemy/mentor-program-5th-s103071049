ac7072310fb7043305633a5c6f2d56567f55d2e4
Liu <gentle9278@gmail.com>
2021-05-09 21:49:22 +0800

    2

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -5,24 +5,34 @@ const process = require('process')
 
 const name = process.argv[2]
 
-request(
+function main() {
+  if (!name) {
+    return console.log('請輸入國家名稱')
+  }
+  request(
   `https://restcountries.eu/rest/v2/name/${name}`,
 
   (error, response, body) => {
-    const json = JSON.parse(body)
-    // eslint-disable-next-line
-    const length = Object.keys(json).length
+    if (response.statusCode === 404) {
+      return console.log('找不到國家資訊')
+    }
+    let json
     try {
-      for (let i = 0; i < length; i++) {
-        console.log('============')
-        console.log(`國家 : ${json[i].name}`)
-        console.log(`首都 : ${json[i].capital}`)
-        console.log(`貨幣 : ${json[i].currencies[0].code}`)
-        console.log(`國碼 : ${json[i].callingCodes}`)
-      }
+      json = JSON.parse(body)
     } catch (error) {
       console.log('找不到國家資訊')
     }
+    // eslint-disable-next-line
+    const length = Object.keys(json).length
+    for (let i = 0; i < length; i++) {
+      console.log('============')
+      console.log(`國家 : ${json[i].name}`)
+      console.log(`首都 : ${json[i].capital}`)
+      console.log(`貨幣 : ${json[i].currencies[0].code}`)
+      console.log(`國碼 : ${json[i].callingCodes}`)
+    }
   }
+  )
+}
 
-)
+main()
