9a8560a5e4d177ec4c56bec4de27027ee7d31763
s103071049 <47899484+s103071049@users.noreply.github.com>
2021-11-21 20:16:15 +0800

    remove console-log

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -6,17 +6,14 @@ export const selectTodosByFilterState = (store) => {
   const filterState = selectFilterState(store);
   switch (filterState) {
     case VISIBILITY_FILTERS.ALL: {
-      console.log("allTodos", allTodos);
       return allTodos;
     }
     case VISIBILITY_FILTERS.COMPLETED: {
       const completed = allTodos.filter((todo) => todo.isDone);
-      console.log("completed", completed);
       return completed;
     }
     case VISIBILITY_FILTERS.ACTIVE: {
       const undo = allTodos.filter((todo) => !todo.isDone);
-      console.log("undo", undo);
       return undo;
     }
     default:
